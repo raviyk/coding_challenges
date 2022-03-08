e6e783f3e0655254d9fc295a44c9a9ed88383a44
Ravi <ravi.yk@gmail.com>
2022-03-08 17:59:29 +0530

    Create sum_first_n_natural_num.py
    Problem Description
    
    Write a program to find sum all Natural numbers from 1 to N where you have to take N as input from user
    
    
    
    Problem Constraints
    
    1 <= N <= 1000
    
    
    
    Input Format
    
    A single line representing N
    
    
    
    Output Format
    
    A single integer showing sum of all Natural numbers from 1 to N
    
    
    
    Example Input
    
    Input 1:
    
    5
    Input 2:
    
    10
    
    
    Example Output
    
    Output 1:
    
    15
    Output 2:
    
    55

---
+++ sum_first_n_natural_num.py
@@ -0,0 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    n = int(input())
+    sum_n = int((n*(n+1))/2)
+    print(sum_n)
+    return 0
+
+if __name__ == '__main__':
+    main()
